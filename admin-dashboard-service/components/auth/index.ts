4237a42ff86642e51c4d08dab5c71581473c3e27
Yorn Thanon <yornthanon.dev@gmail.com>
2026-09-19 21:15:33 +0700

    delete all

--- admin-dashboard-service/components/auth/index.ts
+++
@@ -1,1 +0,0 @@
-export { ProtectedRoute, PublicOnlyRoute, AdminRoute } from './ProtectedRoute';
--- admin-dashboard-service/components/layout/index.ts
+++
@@ -1,3 +0,0 @@
-export { AdminLayout } from './AdminLayout';
-export { StoreLayout } from './StoreLayout';
-export { PageContainer, PageHeader, Section } from './PageContainer';
